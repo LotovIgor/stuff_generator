b9b07bcea5373418ff0acf2e0c06a7a8a908573d
LotovIgor <ilotov@ngs.ru>
2020-10-06 20:11:48 +0700

    generate

--- main.py
+++ main.py
@@ -3,29 +3,21 @@ import random
 # выходной - output
 
 
+# Оля
 def word(str):
     # вход - строка
     # выход - список
-    # Выводит список слов
+    # Выводит список слов в том порядке, в котором они были в тексте
     # return sp
     pass
 
 
+# Арсений
 def unique(sp):
     # вход - список
     # выход - строка
     # находит уникальные слова
     # return un
-    pass
-
-
-def link(sp):
-    # вход - список
-    # выход - словарь(?)
-    # звенья и связи
-    # return sl
-    # в словаре ключи - слова из un
-    # в ключах списки из слов из un, которые с ними связаны
     pass
 
 
@@ -35,6 +27,21 @@ def start(sp):
     # выход - список
     # return st
     pass
+
+
+# Игорь
+def link(sp, un):
+    # вход - список
+    # выход - словарь
+    # звенья и связи
+    # return sl
+    # в словаре ключи - слова из un
+    # в ключах списки из слов из un, которые с ними связаны
+    sl = {i:[] for i in un}
+    # print(sl)
+    for i in range(len(sp) - 1):
+            sl[sp[i]].append(sp[i + 1])
+    return sl
 
 
 def generate(sl, st, n):
@@ -48,7 +55,11 @@ def generate(sl, st, n):
             s = s[:-1]
         s += ' '
         for i in range(19):
-            s += random.choice(sl[w])
+            if sl[w] == []:
+                w = random.choice(st)
+            else:
+                w = random.choice(sl[w])
+            s += w
             if i < 3 and s[-1] in '.!?':
                 s = s[:-1]
             if i >= 3 and s[-1] in '.!?':
@@ -59,5 +70,10 @@ def generate(sl, st, n):
 
 
 if __name__ == '__main__':
-    generate({'g.': ['g.']}, ['g.'], 3)
-    pass
+    # generate({'g.': ['g.']}, ['g.'], 3)
+    sp = ['Hello,', 'my', 'name', 'is', 'Igor.', 'I', 'am', 'Igor.', 'Igor', 'is', 'me.']
+    un = ['Hello,', 'my', 'name', 'is', 'Igor.', 'I', 'am', 'Igor', 'is', 'me.']
+    st = ['Hello,', 'Igor.', 'I', 'Igor']
+    n = 5
+    # print(link(sp, un))
+    generate(link(sp, un), st, n)
